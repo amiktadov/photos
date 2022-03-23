4d13ed9c57a190dd11c60470954a2b29be90c921
user <userexceed@Mac-mini-user.local>
2022-03-23 17:04:10 +0300

    add BaseModel (date, user)

--- app/models.py
+++ app/models.py
@@ -4,16 +4,22 @@ from django.contrib.auth.models import User
 from django.forms import DateTimeField
 
 
+class BaseModel(models.Model):
+    date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+
+    class Meta:
+        abstract = True
+
+
 class Size(models.Model):
     title = models.CharField(max_length=50)
     width = models.IntegerField()
     height = models.IntegerField()
 
 
-class Post(models.Model):
-    date = models.DateTimeField(auto_now_add=True)
+class Post(BaseModel):
     description = models.CharField(max_length=250, blank=True, null=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
 
 
 class Image(models.Model):
@@ -22,7 +28,7 @@ class Image(models.Model):
     coords = models.CharField(max_length=255, blank=True, null=True)
 
 
-class Reaction(models.Model):
+class Reaction(BaseModel):
     CHOICE = (
         ('smile', '😊'),
         ('sad', '☹'),
@@ -30,16 +36,12 @@ class Reaction(models.Model):
         ('angry', '😡'),
         ('hard', '♥')
     )
-    date_add = models.DateTimeField(auto_now_add=True)
     date_del = models.DateTimeField(default=None, null=True)
     emoji = models.CharField(max_length=50, choices=CHOICE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='reactions')
 
 
-class Comment(models.Model):
-    date_add = models.DateTimeField(auto_now_add=True)
+class Comment(BaseModel):
     date_del = models.DateTimeField(default=None, null=True)
     text = models.TextField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
